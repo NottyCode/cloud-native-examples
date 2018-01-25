ac8793f19770ef8b3173f595252d6112a2004d65
Graham Charters <charters@grahams-mbp.hursley.uk.ibm.com>
2018-01-25 10:40:16 +0000

    Add support for MicroProfile Health and Metrics.

--- serviceC/src/main/java/test/ServiceC.java
+++ serviceC/src/main/java/test/ServiceC.java
@@ -1,6 +1,6 @@
 package test;
 
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
@@ -11,15 +11,25 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Gauge;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 
 @Path("props")
-@RequestScoped
+@ApplicationScoped
 public class ServiceC {
     @Inject
     private MyBean bean;
 
     private static transient boolean failSometimes = true;
 
+    @Gauge(name="numProps", displayName="Number of properties", unit="properties", description="The total number of properties")
+    private int numProps() {
+        return bean.getProperties().size();
+    }
+
+    @Timed(name="getPropertyTime", displayName="Call duration", description="Time spend in call")
+    @Counted(name="getPropertyCount", absolute=false, monotonic=true, displayName="Times called", description="Number of times called")
     @Produces(MediaType.APPLICATION_JSON)
     @GET
     @Path("{propName}")
