528b764019ff6d6b710eee370108317ed455f2a0
CHARTERS <charters@uk.ibm.com>
2018-02-20 14:44:35 +0000

    Moved Health reporting to Service A

--- serviceA/src/main/java/test/ServiceAHealth.java
+++ serviceA/src/main/java/test/ServiceAHealth.java
@@ -6,22 +6,32 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.client.WebTarget;
 
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.health.Health;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 
 @Health
 @ApplicationScoped
-public class ServiceCHealth implements HealthCheck {
+public class ServiceAHealth implements HealthCheck {
+
+  @Inject
+  @ConfigProperty(name="serviceC.url")
+  private String serviceCURL;
 
   public boolean isHealthy() {
-    String url = UriBuilder.fromUri("/serviceC/props/host").host("localhost").port(9081).scheme("http").build().toString();
 
-    Client client = ClientBuilder.newClient();
-    Response response = client.target(url).request(MediaType.APPLICATION_JSON)
-                              .get();
-    if (response.getStatus() != 200) {
+    System.out.println(serviceCURL);
+
+    Client c = ClientBuilder.newClient();
+    WebTarget t = c.target(serviceCURL + "/" + "test");
+    Response r = t.request().get();
+
+    if (r.getStatus() != 200) {
       return false;
     }
     return true;
@@ -30,10 +40,10 @@ public class ServiceCHealth implements HealthCheck {
   @Override
   public HealthCheckResponse call() {
     if (!isHealthy()) {
-      return HealthCheckResponse.named(ServiceC.class.getSimpleName())
+      return HealthCheckResponse.named(ServiceA.class.getSimpleName())
                                 .down().build();
     }
-    return HealthCheckResponse.named(ServiceC.class.getSimpleName())
+    return HealthCheckResponse.named(ServiceA.class.getSimpleName())
                               .up().build();
   }
 }
