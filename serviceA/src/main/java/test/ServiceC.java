def1a123473ad2a6f1181901434985a78fba060f
Alasdair Nottingham <alasdair.nottingham@gmail.com>
2018-02-21 13:09:08 -0800

    Remove need for beans.xml

--- serviceA/src/main/java/test/ServiceC.java
+++ serviceA/src/main/java/test/ServiceC.java
@@ -6,10 +6,13 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.annotation.ManagedBean;
+import javax.enterprise.context.Dependent;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @RegisterRestClient
 @Path("props")
+@Dependent
 public interface ServiceC {
 
     @GET
